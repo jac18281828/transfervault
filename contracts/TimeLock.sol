6e5441aeef8e1feaf6eeae7bebaf6b7b5231bcb6
John Cairns <john@2ad.com>
2022-10-04 11:00:40 -0500

    Timelock is working all tests passing

--- contracts/TimeLock.sol
+++ contracts/TimeLock.sol
@@ -3,7 +3,126 @@ pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// solhint-disable no-empty-blocks
-contract TimeLock is Ownable {
+// based on https://github.com/compound-finance/compound-protocol/blob/a3214f67b73310d547e00fc578e8355911c9d376/contracts/Timelock.sol
 
+contract TimeLock is Ownable {
+    event CancelTransaction(
+        bytes32 indexed txHash,
+        address indexed target,
+        uint256 value,
+        string signature,
+        bytes data,
+        uint256 eta
+    );
+    event ExecuteTransaction(
+        bytes32 indexed txHash,
+        address indexed target,
+        uint256 value,
+        string signature,
+        bytes data,
+        uint256 eta
+    );
+    event QueueTransaction(
+        bytes32 indexed txHash,
+        address indexed target,
+        uint256 value,
+        string signature,
+        bytes data,
+        uint256 eta
+    );
+
+    uint256 public constant GRACE_PERIOD = 14 days;
+    uint256 public constant MINIMUM_DELAY = 2 days;
+    uint256 public constant MAXIMUM_DELAY = 30 days;
+
+    uint256 public _lockTime;
+
+    mapping(bytes32 => bool) public _queuedTransaction;
+
+    constructor(uint256 _lock) {
+        require(_lock >= MINIMUM_DELAY, "Delay too short");
+        require(_lock <= MAXIMUM_DELAY, "Delay too long");
+        _lockTime = _lock;
+    }
+
+    function queueTransaction(
+        address target,
+        uint256 value,
+        string memory signature,
+        bytes memory data,
+        uint256 eta
+    ) external onlyOwner returns (bytes32) {
+        require(
+            eta >= (getBlockTimestamp() + _lockTime),
+            "Scheduled during time lock"
+        );
+
+        bytes32 txHash = keccak256(
+            abi.encode(target, value, signature, data, eta)
+        );
+        _queuedTransaction[txHash] = true;
+
+        emit QueueTransaction(txHash, target, value, signature, data, eta);
+        return txHash;
+    }
+
+    function cancelTransaction(
+        address target,
+        uint256 value,
+        string memory signature,
+        bytes memory data,
+        uint256 eta
+    ) external onlyOwner {
+        bytes32 txHash = keccak256(
+            abi.encode(target, value, signature, data, eta)
+        );
+        _queuedTransaction[txHash] = false;
+
+        emit CancelTransaction(txHash, target, value, signature, data, eta);
+    }
+
+    function executeTransaction(
+        address target,
+        uint256 value,
+        string memory signature,
+        bytes memory data,
+        uint256 eta
+    ) external payable onlyOwner returns (bytes memory) {
+        bytes32 txHash = keccak256(
+            abi.encode(target, value, signature, data, eta)
+        );
+        require(_queuedTransaction[txHash], "Not queued");
+        require(getBlockTimestamp() >= eta, "Transaction is locked");
+        require(
+            getBlockTimestamp() <= (eta + GRACE_PERIOD),
+            "Transaction is stale."
+        );
+
+        _queuedTransaction[txHash] = false;
+
+        bytes memory callData;
+
+        if (bytes(signature).length == 0) {
+            callData = data;
+        } else {
+            callData = abi.encodePacked(
+                bytes4(keccak256(bytes(signature))),
+                data
+            );
+        }
+
+        // solhint-disable-next-line security/no-call-value
+        (bool success, bytes memory returnData) = target.call{value: value}(
+            callData
+        );
+        require(success, "Execution reverted.");
+
+        emit ExecuteTransaction(txHash, target, value, signature, data, eta);
+
+        return returnData;
+    }
+
+    function getBlockTimestamp() internal view returns (uint256) {
+        return block.timestamp;
+    }
 }
